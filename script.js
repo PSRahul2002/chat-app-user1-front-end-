c859f510b3f544e03241516276c018fc5ddf7054
PSRahul2002 <86890267+PSRahul2002@users.noreply.github.com>
2024-10-11 23:59:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function updateClock() {
   clockElement.textContent = `${hours}  :  ${minutes}  :  ${seconds}`;
 
   // Define the time range (for example, 09:00 to 17:00)
-  const startHour = 9;
-  const endHour = 20;
+  const startHour = 0;
+  const endHour = 2;
 
   // Logic to show/hide button or message
   if (now.getHours() >= startHour && now.getHours() < endHour) {
